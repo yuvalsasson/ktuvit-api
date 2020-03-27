a05e9fa99c671a0180eafbcd909a1fca63e32155
Yuval <private@me.com>
2020-03-27 18:44:55 +0300

    Added support for all methods and exception handling

--- ktuvit.py
+++ ktuvit.py
@@ -1,4 +1,8 @@
 import requests
+import json
+
+class ApiError(Exception):
+	pass
 
 class KtuvitApi(object):
 	"""docstring for KtuvitApi"""
@@ -8,6 +12,55 @@ class KtuvitApi(object):
 		self.protocol = protocol
 		self.domain = domain
 
-	def api_call(url, params):
+	def do_json_api(self, path, params):
+		response = self.do_api(path, params)
+		# there is a bug in the api: The response is json of json. Therefore we need to decode it twice.
+		result = json.loads(response.content)
 
-	def find_film(search_type, pharse, version="1.0"):
+		# result may be encoded second time
+		try:
+			result = json.loads(result)
+		except json.JSONDecodeError:
+			pass
+
+		if result["IsSuccess"] == False or result['ErrorMessage'] is not None:
+			raise ApiError(result["ErrorMessage"])
+		return result
+
+	def do_api(self, path, params):
+		url = f"{self.protocol}://{self.domain}/{path}"
+		headers = {
+			"Content-Type" : "application/json"
+
+		}
+		query={
+			"request":params
+		}
+		data = json.dumps(query)
+		return requests.post(url, headers=headers, data=data)
+
+		
+	def find_film(self, search_type, phrase, version="1.0"):
+		return self.do_json_api("FindFilm", {"SearchType" : search_type, "SearchPhrase" : phrase, "Version" : "1.0"})
+
+	def find_series(self, search_type, phrase, season, episode, version='1.0'):
+		return self.do_json_api("FindSeries", {
+			"SearchType":search_type,
+			'SearchPhrase':phrase,
+			'Season':str(season),
+			'Episode':str(episode),
+			'Version':version
+			})
+
+	def download_subtitle(self, subtitle_id, font_size=None, hex_color=None):
+		#TODO: Add support for exceptions 
+		params = {
+			"subtitleID" : subtitle_id # Documentation is wrong, param name starts with lowercase 's'
+		}
+		if font_size is not None:
+			params["FontSize"] = int(font_size)
+		
+		if hex_color is not None:
+			params["HexColor"] = str(hex_color)
+		
+		return self.do_api("Download", params)
