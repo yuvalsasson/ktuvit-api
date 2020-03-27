55e08e8f1376b1b152067f0a6131865110f81130
Yuval <private@me.com>
2020-03-27 19:20:48 +0300

    updated setup.py with the correct license

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ],
     python_requires='>=3.6',
 )
